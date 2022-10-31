dc833d3eb4a93396681602b1375e18b47ab8ddd9
Сасов Дмитрий Александрович <SasovDA@sv.com>
2022-10-31 16:36:08 +0300

    Добавлена коллекция, куда будут сохраняться Articles

--- blog-service-infrastructure-database-mongo/src/main/java/org/example/article/documents/ArticleDoc.java
+++ blog-service-infrastructure-database-mongo/src/main/java/org/example/article/documents/ArticleDoc.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
+@Document(collection = "articles")
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
